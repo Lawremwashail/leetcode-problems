3c26d23ccb4d48e910bd694c7783001dc8c922c4
Lawremwashail <lawremwash@gmail.com>
2025-01-05 11:33:26 +0300

    remove

---
+++ javadir/removeelement/Main.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        Remove element = new Remove();
+        int nums[] = {0, 1, 1, 3, 2, 2, 0, 4, 3, 2};
+        int val = 2;
+
+        element.removeElement(nums, val);
+
+
+        
+    }
+}
---
+++ javadir/removeelement/Remove.java
@@ -0,0 +1,16 @@
+// Remove elements from an array
+class Remove {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+
+        }
+        return k;
+    }
+}
+
